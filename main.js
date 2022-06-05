7550bac7afcee1f8b7d4aac05c25abadd9fc087d
suryasanZ <suryasanjiwani@student.telkomuniversity.ac.id>
2022-06-05 18:49:12 +0700

    first commit22

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 setInterval(function () {
-    fetch("http://localhost/api")
+    fetch("http://https://smart-sec.herokuapp.com/api")
       .then((hasil) => hasil.json())
       .then((res) => {
         console.log(res);
         document.getElementById("deteksi").innerHTML = res.Ada_Pergerakan;
 
+        
       });
   }, 1000);
